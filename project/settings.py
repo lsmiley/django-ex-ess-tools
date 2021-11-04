42ac288e67b1ec28c1acc206762c3f3dadb1dba9
lsmiley <lsmiley@us.ibm.com>
2021-11-04 18:30:26 -0400

    DBErrorsFix

--- project/settings.py
+++ project/settings.py
@@ -82,10 +82,20 @@ WSGI_APPLICATION = 'wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
-from . import database
+# from . import database
+#
+# DATABASES = {
+#     'default': database.config()
+# }
+
+# Database
+# https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': database.config()
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
 }
 
 
